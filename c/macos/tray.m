012cd00c094102bfc0af618165f9ecb384d0a1ec
Sylvester Stephenson <sylvesterstephenson028@gmail.com>
2023-09-28 22:32:28 +0200

    Reworked code based on the V example.

--- c/macos/tray.m
+++ c/macos/tray.m
@@ -1,84 +1,110 @@
 #ifdef __APPLE__
-#include "tray.h"
-
-void *GLOBAL_TRAY = NULL;
 
 static NSString *nsstring(char* c_string {
   return [NSString stringWithUTF8String:c_string];
 }
 
-// Callback for the tray icon click
-void on_menu_item_clicked(NSMenuItem *menuItem) {
-    MenuItemMac *item = (MenuItemMac *)menuItem.representedObject;
-    VTray *tray = (__bridge VTray *)get_global_vtray();
-    if (tray != nil) {
-        const char *itemId = [item.text UTF8String];
-        tray->on_click(itemId);
-    } else {
-        NSLog(@"Global pointer is NULL");
-    }
+// Manages the app lifecycle.
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+  NSAutoreleasePool *pool;
+  NSApplication *app;
+  NSStatusItem *statusItem;    // our button
+  main__VTrayParamsMac trayParams; // VTrayParamsMac is defined in tray.v
+}
+@end
+
+@implementation AppDelegate
+- (AppDelegate *)initWithParams:(main__VTrayParamsMac) params {
+  if (self = [super init]) {
+    trayParams = params;
+  }
+  return self;
 }
 
-
-// Create and initialize your VTray instance
-struct VTray *vtray_init_mac(VTrayParamsMac *params, size_t num_items, MenuItemMac *items[]) {
-    struct VTray *tray = (struct VTray *)malloc(sizeof(struct VTray));
-    if (!tray) {
-        // Handle allocation failure
-        fprintf(stderr, "Unable to allocate memory for VTray!\n");
-        exit(1);
-    }
-
-    // Initialize the NSApplication
-    [NSApplication sharedApplication];
-
-    // Create a status item in the system menu bar
-    tray->statusItem = [NSStatusBar.systemStatusBar statusItemWithLength:NSVariableStatusItemLength];
-    // Deprecated
-    tray->statusItem.toolTip = [NSString stringWithUTF8String:nsstring(params->tooltip)];
-    tray->statusItem.button.image = [NSImage imageNamed:[NSString stringWithUTF8String:nsstring(params->icon)]];
-
-    // Create a menu
-    tray->menu = [[NSMenu alloc] initWithTitle:nsstring(params->tooltip)];
-    vtray_construct(items, num_items, tray);
-    tray->statusItem.menu = tray->menu;
-
-    // Set the callback for menu item clicks
-    [NSApp setDelegate:(id<NSApplicationDelegate>)tray];
-    [NSApp run];
-
-    return tray;
+// Called when NSMenuItem is clicked.
+- (void)onAction:(id)sender {
+  struct main__MenuItemMac *item =
+      (struct main__MenuItemMac *)[[sender representedObject] pointerValue];
+  if (item) {
+    trayParams->on_click(*item->id);
+  }
 }
 
+- (NSMenu *)buildMenu {
+  NSMenu *menu = [NSMenu new];
+  [menu autorelease];
+  [menu setAutoenablesItems:NO];
 
-void vtray_construct(MenuItemMac *items[], size_t num_items, struct VTray *parent) {
-    if (parent->menu) {
-        for (size_t i = 0; i < num_items; i++) {
-            MenuItemMac *item = items[i];
-            NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:[NSString stringWithUTF8String:nsstring(item->text)] action:@selector(vtray_on_click:) keyEquivalent:@""];
-            menuItem.tag = item->id;
-            [menuItem setTarget:nil];
-            [parent->menu addItem:menuItem];
-        }
-    }
-    // Attach the menu to the status item
-    [parent->statusItem setMenu:parent->menu];
+  main__MenuItemMac *params_items = trayParams.items.data;
+  for (int i = 0; i < trayParams.items.len; i++) {
+    NSString *title = nsstring(params_items[i]->text);
+    NSMenuItem *item = [menu addItemWithTitle:title
+                                       action:@selector(onAction:)
+                                keyEquivalent:@""];
+    NSValue *representedObject = [NSValue valueWithPointer:(params_items + i)];
+    [item setRepresentedObject:representedObject];
+    [item setTarget:self];
+    [item autorelease];
+    [item setEnabled:YES];
+  }
+
+  return menu;
 }
 
-void vtray_run_mac(struct VTray *tray) {
-  NSApplication *app = (NSApplication *)(tray->app);
+- (void)initTrayMenuItem {
+  NSStatusBar *statusBar = [NSStatusBar systemStatusBar];
+  statusItem = [statusBar statusItemWithLength:NSSquareStatusItemLength];
+  [statusItem retain];
+  [statusItem setVisible:YES];
+  NSStatusBarButton *statusBarButton = [statusItem button];
+
+  // Height must be 22px.
+  NSImage *img = [NSImage imageNamed:trayParams->icon];
+  [statusBarButton setImage:img];
+  NSMenu *menu = [self buildMenu];
+  [statusItem setMenu:menu];
+}
+
+- (void)applicationWillFinishLaunching:(NSNotification *)notification {
+  NSLog(@"applicationWillFinishLaunching called");
+}
+
+- (void)applicationWillTerminate:(NSNotification *)notif;
+{ NSLog(@"applicationWillTerminate called"); }
+
+- (NSApplicationTerminateReply)applicationShouldTerminate:
+    (NSApplication *)sender {
+  NSLog(@"applicationShouldTerminate called");
+  return NSTerminateNow;
+}
+@end
+
+// Initializes NSApplication and NSStatusItem, aka system tray menu item.
+main__VTray *vtray_init_mac(main__VTrayParamsMac *params) {
+  NSApplication *app = [NSApplication sharedApplication];
+  AppDelegate *appDelegate = [[AppDelegate alloc] initWithParams:params];
+
+  // Hide icon from the doc.
+  [app setActivationPolicy:NSApplicationActivationPolicyProhibited];
+  [app setDelegate:appDelegate];
+
+  [appDelegate initTrayMenuItem];
+
+  main__VTray *tray = malloc(sizeof(main__VTray));
+  tray->ptr = app;
+  tray->ptr_delegate = appDelegate;
+  return tray;
+}
+
+// Blocks and runs the application.
+void vtray_run_mac(main__VTray *tray) {
+  NSApplication *app = (NSApplication *)(tray->ptr);
   [app run];
 }
 
-
-// Clean up and exit the application
-void vtray_exit_mac(struct VTray *tray) {
-    // Release resources, dealloc, and exit
-    if (tray) {
-        [NSStatusBar.systemStatusBar removeStatusItem:tray->statusItem];
-        free(tray);
-        [NSApp terminate:nil];
-        exit(1);
-    }
+// Terminates the app.
+void vtray_exit_windows(main__VTray *tray) {
+  NSApplication *app = (NSApplication *)(tray->ptr);
+  [app terminate:app];
 }
 #endif
