19364f1b50b929712fcfa99b1eea94f75d741756
Sylvester Stephenson <sylvesterstephenson028@gmail.com>
2023-10-06 11:25:05 -0700

    Reworked logic on MacOS, this allows the callback function to return a struct of VTrayMenuItem instead of an ID.

--- c/macos/tray.m
+++ c/macos/tray.m
@@ -1,15 +1,43 @@
 #ifdef __APPLE__
-#include <stdlib.h>
 
 /**
  * This code was based off of https://github.com/vlang/v/blob/master/examples/macos_tray/tray.m
  * All credit goes to the legend who created this.
  */
 
+static char *string_to_char(string str)
+{
+    if (str.str == NULL)
+    {
+        perror("string_to_char: string is NULL");
+        return NULL;
+    }
+    return str.str;
+}
 
-static NSString *nsstring(char* s) {
-    NSString *myNSString = [NSString stringWithUTF8String:s];
-    return myNSString;
+static size_t len(string str)
+{
+    return str.len;
+}
+
+static char *char_to_nsstring(char *str)
+{
+    if (str == NULL)
+    {
+        perror("char_to_nsstring: string is NULL");
+        return NULL;
+    }
+    return [NSString stringWithUTF8String:str];
+}
+
+static NSString *string_to_nsstring(string str)
+{
+    if (str.str == NULL)
+    {
+        perror("string_to_nsstring: string is NULL");
+        return NULL;
+    }
+    return [NSString stringWithUTF8String:str.str];
 }
 
 // Manages the app lifecycle.
@@ -18,14 +46,14 @@ static NSString *nsstring(char* s) {
   NSApplication *app;
   NSStatusItem *statusItem;    // our button
   int num_items;
-  vtray__VTrayParamsMac *trayParams; // VTrayParamsMac is defined in tray.v
-  vtray__MenuItemMac **menuItems;
+  vtray__VTrayParams *trayParams; // VTrayParams is defined in tray.v
+  vtray__VTrayMenuItem **menuItems;
 }
 @end
 
 @implementation AppDelegate
-- (AppDelegate *)initWithParams:(vtray__VTrayParamsMac *)params
-                        itemsArray:(vtray__MenuItemMac *[])itemsArray
+- (AppDelegate *)initWithParams:(vtray__VTrayParams *)params
+                        itemsArray:(vtray__VTrayMenuItem *[])itemsArray
                     numItems:(int)numItems {
   if (self = [super init]) {
     trayParams = params;
@@ -40,10 +68,10 @@ static NSString *nsstring(char* s) {
 
 // Called when NSMenuItem is clicked.
 - (void)onAction:(id)sender {
-  struct vtray__MenuItemMac *item =
-      (struct vtray__MenuItemMac *)[[sender representedObject] pointerValue];
+  struct vtray__VTrayMenuItem *item =
+      (struct vtray__VTrayMenuItem *)[[sender representedObject] pointerValue];
   if (item) {
-    trayParams->on_click(item->id);
+    trayParams->on_click(item);
   }
 }
 
@@ -53,7 +81,7 @@ static NSString *nsstring(char* s) {
   [menu setAutoenablesItems:NO];
 
   for (int i = 0; i < num_items; i++) {
-    NSString *title = nsstring(menuItems[i]->text);
+    NSString *title = string_to_nsstring(menuItems[i]->text);
     NSMenuItem *item = [menu addItemWithTitle:title
                                        action:@selector(onAction:)
                                 keyEquivalent:@""];
@@ -74,9 +102,9 @@ static NSString *nsstring(char* s) {
   [statusItem setVisible:YES];
   NSStatusBarButton *statusBarButton = [statusItem button];
   // Height must be 22px.
-    NSImage *img = [[NSImage alloc] initWithContentsOfFile:nsstring(trayParams->icon)];
+    NSImage *img = [[NSImage alloc] initWithContentsOfFile:string_to_nsstring(trayParams->icon)];
   if (img == nil) {
-    NSLog(@"Error loading image: %@", nsstring(trayParams->icon));
+    NSLog(@"Error loading image: %@", string_to_nsstring(trayParams->icon));
     // Check the image path and file as well as other potential issues.
     } else {
         [statusBarButton setImage:img];
@@ -93,7 +121,7 @@ static NSString *nsstring(char* s) {
 @end
 
 // Initializes NSApplication and NSStatusItem, aka system tray menu item.
-vtray__VTray *vtray_init_mac(vtray__VTrayParamsMac *params, int numItems, vtray__MenuItemMac *itemsArray[]) {
+vtray__VTray *vtray_init(vtray__VTrayParams *params, int numItems, vtray__VTrayMenuItem *itemsArray[]) {
   NSApplication *app = [NSApplication sharedApplication];
   AppDelegate *appDelegate = [[AppDelegate alloc] initWithParams:params itemsArray:itemsArray numItems:numItems];
 
@@ -110,14 +138,15 @@ vtray__VTray *vtray_init_mac(vtray__VTrayParamsMac *params, int numItems, vtray_
 }
 
 // Blocks and runs the application.
-void vtray_run_mac(vtray__VTray *tray) {
+void vtray_run(vtray__VTray *tray) {
   NSApplication *app = (NSApplication *)(tray->ptr);
   [app run];
 }
 
 // Terminates the app.
-void vtray_exit_mac(vtray__VTray *tray) {
+void vtray_exit(vtray__VTray *tray) {
   NSApplication *app = (NSApplication *)(tray->ptr);
   [app terminate:app];
 }
+
 #endif
