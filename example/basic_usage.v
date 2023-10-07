537dea9681ff2855b3de94fd7272e3d5603152e5
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-07 15:34:21 +0200

    Update `example/basic_usage.v`

--- example/basic_usage.v
+++ example/basic_usage.v
@@ -2,12 +2,6 @@ module main
 
 import ouri028.vtray
 
-enum MenuItems {
-	edit = 1
-	copy = 2
-	quit = 3
-}
-
 fn main() {
 	icon := $if macos {
 		'${@VMODROOT}/assets/icon.png'
@@ -18,30 +12,23 @@ fn main() {
 		identifier: 'VTray!'
 		tooltip: 'VTray Demo!'
 		icon: icon
-		items: [
-			&vtray.VTrayMenuItem{
-				id: int(MenuItems.edit)
-				text: 'Edit'
-				checkable: true
-			},
-			&vtray.VTrayMenuItem{
-				id: int(MenuItems.copy)
-				text: 'Copy'
-				disabled: true
-			},
-			&vtray.VTrayMenuItem{
-				id: int(MenuItems.quit)
-				text: 'Quit'
-			},
-		]
 	}
-	on_click := fn [systray] (mut menu_item vtray.VTrayMenuItem) {
-		println(menu_item)
-		if menu_item.id == int(MenuItems.quit) {
-			systray.destroy()
-		}
-	}
-	systray.on_click = on_click
+	systray.items = [
+		&vtray.VTrayMenuItem{
+			text: 'Edit'
+			checkable: true
+		},
+		&vtray.VTrayMenuItem{
+			text: 'Copy'
+			disabled: true
+		},
+		&vtray.VTrayMenuItem{
+			text: 'Quit'
+			on_click: fn [systray] () {
+				systray.destroy()
+			}
+		},
+	]
 	systray.vtray_init()
 	systray.run()
 	systray.destroy()
